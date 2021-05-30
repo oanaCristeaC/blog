dfd73ff5295d060ad3b726aa790e4fb1e1d85d93
Ionela Cristea <oanacristea.1007@yahoo.com>
2021-05-30 20:42:01 +0200

    small refactoring

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post extends Model
 {
