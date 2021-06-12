719194af5c43a12037b18d33e5524acc4b9cb3a9
Ionela Cristea <oanacristea.1007@yahoo.com>
2021-06-12 23:51:20 +0200

    mass assignment restrictions opptions

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Everything is Protected from mass assignment
+    // protected $guarded = ['*'];
+
+    // Nothing is protected from mass assignment
+    // protected $guarded = [];
+
+    // Some fields are protected from mass assignment
+    //protected $guarded = ['id'];
+
+    // Some fields are allowed to be inserted as mass assignment
+     protected $fillable= ['title', 'body', 'excerpt'];
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,7 @@
         <article>
             <h2>
                 <a href="/posts/{{ $post->id }}">
-                  {{--  $post->title='hello <script>alert("hello");</script>'--}}
-                    {!! $post->title !!}
+                    {{ $post->title }}
                 </a>
             </h2>
             <p>{{ $post->excerpt }}</p>
