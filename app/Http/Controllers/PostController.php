4358b2af2b42b175756643fde290b840e81e3b6a
oanaCristeaC <oanacristea.1007@yahoo.com>
2021-08-23 18:40:52 +0200

    use names conventions for controllers and views: posts.index and posts.show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
 
     public function show (Post $post)
     {
-        return view('post', [
+        return view('posts.show', [
             'post' => $post,
         ]);
 
