34eead6cf0bddcf4619a1a3e17670ec82c512af4
oanaCristeaC <oanacristea.1007@yahoo.com>
2021-09-06 17:40:15 +0200

    added query strings to the paginate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     {
         return view('posts.index', [
             'posts' => Post::with('category', 'author')
-                    ->filter(request(['search', 'category', 'author']))->paginate(10)
+                    ->filter(request(['search', 'category', 'author']))->paginate(10)->withQueryString()
         ]);
     }
 
